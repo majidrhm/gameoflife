a69ae0b056dd853d1f51d74ff8cb3b3308ece6d9
majidrhm <85577127+majidrhm@users.noreply.github.com>
2021-06-12 02:45:35 +0400

    Create cell.c

---
+++ game/cell.c
@@ -0,0 +1,26 @@
+#include "cell.h"
+
+
+
+void bearCell(Cell *pCell) {
+    pCell->life = 1;
+}
+
+
+
+Cell *newCell(int x, int y) {
+    Cell *pCell = (Cell *) malloc(sizeof(Cell));
+    
+    pCell->x = x;
+    pCell->y = y;
+    pCell->life = 0;
+
+
+    return pCell;
+}
+
+void killCell(Cell *pCell) {
+    if ( pCell != NULL ) {
+        pCell->life = 0;
+    } 
+}
