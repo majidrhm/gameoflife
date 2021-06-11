f399ce121d0a80278f4c8e179c2807d3d69d2f70
Huseynnaghi <85671866+Huseynnaghi@users.noreply.github.com>
2021-06-12 02:48:33 +0400

    Add files via upload

---
+++ draw/cunit_tests.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include"ansi.h"
+#include"sdl.h"
+
+#include<CUnit/Basic.h>
+#include<CUnit/Console.h>
+#include<CUnit/CUnit.h>
+
+
+
+
+void get_console_size_test(void) {
+    int width;
+    int height;
+    get_console_size(&width,&height);
+    CU_ASSERT_NOT_EQUAL(width, 0);
+    CU_ASSERT_NOT_EQUAL(height, 0);
+    CU_ASSERT_NOT_EQUAL(width, -1);
+    CU_ASSERT_NOT_EQUAL(height, -1);
+    
+}
+
+
+
+int main () {
+
+
+    if(CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    CU_pSuite pS1 = CU_add_suite("Testing SDL", NULL, NULL);
+    
+
+    CU_pSuite pS2 = CU_add_suite("Testing ANSI", NULL, NULL);
+    CU_ADD_TEST(pS2, get_console_size_test);
+    
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+
+
+    return CU_get_number_of_failures();
+}
---
+++ draw/sdl.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdint.h>
+#include <time.h>
+#include <SDL2/SDL.h>
+#include "celllist.h"
+
+
+
+
+/// Draw given celllist in a separate window using "SDL"
+/// @param pCellList Pointer to a list to be drawn
+/// @param renderer SDL Renderer
+void draw(SDL_Renderer* renderer, CellList* pCellList);
