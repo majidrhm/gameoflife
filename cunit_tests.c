3c9c90374a688837256a3c4a18d81e863546cd5a
vasifsh <85770045+vasifsh@users.noreply.github.com>
2021-06-12 03:07:04 +0400

    Add files via upload

---
+++ cunit_tests.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"celllist.h"
+#include<CUnit/Console.h>
+#include<CUnit/CUnit.h>
+#include<CUnit/Basic.h>
+
+
+int main () {
+
+
+
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,33 @@
+#include "ansi.h"
+#include <time.h>
+
+int main () {
+    struct timespec ts;
+    ts.tv_nsec = 100000000;
+    int h, w;
+    get_console_size(&w, &h);
+    CellList *pCellList = generateCellList(h,w/2);
+    drawAnsi(pCellList);
+    CellList *pNewCellList;
+    CellList *pOldCellList;
+    
+    
+    while (nAlives(pCellList) > 0) {
+       
+        nanosleep(&ts, NULL);
+        pNewCellList = updateCellList(pCellList);
+        if(sameCellList(pCellList, pNewCellList) == 1 || sameCellList(pOldCellList, pNewCellList) == 1)
+            break;
+        else {
+            drawAnsi(pNewCellList);
+            pOldCellList = copyCellList(pCellList);
+            pCellList = copyCellList(pNewCellList);
+        }
+    }
+    
+    emptyCellList(pNewCellList);
+    drawAnsi(pNewCellList);
+
+    return 0;
+
+}
